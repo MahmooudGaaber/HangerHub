1d9c230488401ce9f8a13b58cbf0357177f6e72b
MahmooudGaaber <mahmoud.gr231@gmail.com>
2025-12-28 10:13:27 +0200

    first commit

---
+++ src/main/java/com/app/hangerhub/HangerHubApplication.java
@@ -0,0 +1,13 @@
+package com.app.hangerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangerHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangerHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/hangerhub/HangerHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hangerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangerHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
